386e05282862e04c7cdb2baf8671e8571d88dacf
Emmytweg <emmanuelolamide706@gmail.com>
2025-05-15 23:39:15 +0100

    ...

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['http://172.20.10.5', 'http://localhost:3000/'], // or whatever IP is shown
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Signup() {
   const [message, setMessage] = useState('');
 
   const handleSignup = async () => {
-    const res = await fetch('https://login-testing-backend.up.railway.app/', {
+    const res = await fetch('https://login-testing-backend.up.railway.app/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
@@ -20,8 +20,8 @@ export default function Signup() {
   return (
     <div className="p-10">
       <h1 className="text-xl font-bold">Signup</h1>
-      <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" className="border p-2 my-2 block" />
-      <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" className="border p-2 my-2 block" />
+      <input value={email} id='' onChange={e => setEmail(e.target.value)} placeholder="Email" className="border p-2 my-2 block" />
+      <input value={password} id='' onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" className="border p-2 my-2 block" />
       <button onClick={handleSignup} className="bg-blue-600 text-white p-2">Register</button>
       {message && <p className="mt-2 text-green-600">{message}</p>}
     </div>
